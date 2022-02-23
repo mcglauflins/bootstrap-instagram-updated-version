d260f37816dcbc4b2dc0148ddeb6154b71d00858
mcglauflins <sarahriebschlager@gmail.com>
2022-02-23 17:19:26 +0000

    java update

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-function gridOrDetail() {
-    let isGrid = document.getElementById("btnradio1");
-    let cardBodies = document.querySelectorAll("#card-container .card-body");
-    let cards = document.querySelectorAll(".card");
-    let container = document.querySelector("#card-container");
-  
-    if (isGrid.checked) {
-      container.classList.replace("flex-column", "flex-row");
-      container.classList.replace("flex-nowrap", "flex-wrap");
-      container.classList.remove('align-items-center');
-      for (cardBody of cardBodies) {
-        cardBody.style.display = "none";
-      }
-      for (card of cards) {
-        card.style.width = "18rem";
-      }
-    } else {
-      container.classList.replace("flex-row", "flex-column");
-      container.classList.replace("flex-wrap", "flex-nowrap");
-      container.classList.add('align-items-center');
-      for (cardBody of cardBodies) {
-        cardBody.style.display = "block";
-      }
-      for (card of cards) {
-        card.style.width = "36rem";
-      }
-    }
-  }
+function handleRadioMenuClick(event) {
+  document.querySelectorAll(".radio-menu-container").forEach(elm => elm.classList.add("d-none"))
+  document.querySelector(event.target.getAttribute("data-target")).classList.remove("d-none")
+  console.log("radioMenuInputwasclicked")
+}
   
   // this should be on a window.onload
-  document.getElementById("btnradio1").addEventListener("click", gridOrDetail);
-  document.getElementById("btnradio2").addEventListener("click", gridOrDetail);
-  gridOrDetail();
-  
+ window.onload = function(){ 
+  document.querySelectorAll(".radio-menu label").forEach(elm => elm.addEventListener("click", handleRadioMenuClick));
+}
